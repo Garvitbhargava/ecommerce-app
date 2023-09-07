98c2795bc542a8c3f9c9ccf8cbfb146d0ef4dced
Garvit Bhargava <77270135+Garvitbhargava@users.noreply.github.com>
2023-09-07 11:58:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
   <BrowserRouter>
   <Routes>
     <Route path='/' element={<Layout/>}>
+     <Route index element={<Home />} />
        <Route index path='home' element={<Home />} />
        <Route path='about' element={<About />} />
             <Route path='contact' element={<Contact />} />
